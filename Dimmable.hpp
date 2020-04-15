1590ccc621ff6d2671189aeb54ef9331f1c38025
Volkan Orhan <contactme@volkanorhan.com>
2020-04-15 12:05:48 +0300

    no message

--- Dimmable.hpp
+++ Dimmable.hpp
@@ -5,19 +5,19 @@ class Dimmable
 {
 public:
     Dimmable();
-    inline unsigned short getDimmingPercentage() const;
-    inline void setDimmingPercentage(unsigned short value);
+    inline unsigned char getDimmingPercentage() const;
+    inline void setDimmingPercentage(unsigned char value);
 
 private:
     unsigned short dimmingPercentage;
 };
 
-inline unsigned short Dimmable::getDimmingPercentage() const
+inline unsigned char Dimmable::getDimmingPercentage() const
 {
     return dimmingPercentage;
 }
 
-inline void Dimmable::setDimmingPercentage(unsigned short value)
+inline void Dimmable::setDimmingPercentage(unsigned char value)
 {
     dimmingPercentage = value;
 }
