e84b8b4d5f0d661054a02f1c647e09dc21e76aa7
Volkan Orhan <contactme@volkanorhan.com>
2020-05-16 21:38:43 +0300

    no message

--- src/PeakMark.hpp
+++ src/PeakMark.hpp
@@ -1,20 +1,28 @@
 #ifndef PEAKMARK_HPP
 #define PEAKMARK_HPP
+#include <algorithm>
 
 template<class T>
 class PeakMark
 {
 public:
     PeakMark();
-    inline void setPeakMark(T enabled);
+    inline void setPeakMarkEnabled(bool enabled);
+    bool isPeakMarkEnabled() const;
+
     inline void setPeakMarkValue(T value);
+    T getPeakMarkValue() const;
+
+    inline size_t getLifeTime() const;
+    inline void setLifeTime(const size_t lifeTimeMiliseconds);
 private:
     T peakMarkValue;
-    T peakMarkEnabled = false;
+    bool peakMarkEnabled = false;
+    size_t lifeTime;
 };
 
 template<class T>
-inline void PeakMark<T>::setPeakMark(T enabled)
+inline void PeakMark<T>::setPeakMarkEnabled(bool enabled)
 {
     peakMarkEnabled = enabled;
 }
@@ -23,6 +31,30 @@ template<class T>
 inline void PeakMark<T>::setPeakMarkValue(T value)
 {
     peakMarkValue = value;
+}
+
+template<class T>
+inline size_t PeakMark<T>::getLifeTime() const
+{
+    return lifeTime;
+}
+
+template<class T>
+inline void PeakMark<T>::setLifeTime(const size_t lifeTimeMiliseconds)
+{
+    lifeTime = lifeTimeMiliseconds;
+}
+
+template<class T>
+inline T PeakMark<T>::getPeakMarkValue() const
+{
+return peakMarkValue;
+}
+
+template<class T>
+inline bool PeakMark<T>::isPeakMarkEnabled() const
+{
+    return peakMarkEnabled;
 }
 
 #endif // PEAKMARK_HPP
