85b84ba227d49393eab1bd4ef6676d669bfc2c50
Volkan Orhan <volkanorhan@modplugplayer.org>
2021-12-23 01:57:00 +0300

    License information has been added

--- test/MainWindow.cpp
+++ test/MainWindow.cpp
@@ -1,6 +1,16 @@
 #include "MainWindow.hpp"
 #include "ui_MainWindow.h"
 #include "../src/SpectrumAnalyzer.hpp"
+/*
+MainWindow class definitions for the tests of Spectrum Analyzer
+Copyright (C) 2020 Volkan Orhan
+
+This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation; either version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -136,4 +146,3 @@ void MainWindow::on_pushButton20_clicked()
 {
     setValue(-20.0);
 }
-
